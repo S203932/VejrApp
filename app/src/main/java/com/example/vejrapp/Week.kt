a3d2a0a6f091b58efb8c4b9809248dce7700c9f5
Davidgtrivera <s215461@dtu.dk>
2023-11-02 16:15:26 +0100

    Dummy search bar, DayPage & Week modifications
    Added a dummy search bar for previews, DayPage added padding and week is restructured

--- app/src/main/java/com/example/vejrapp/Week.kt
+++ app/src/main/java/com/example/vejrapp/Week.kt
@@ -4,12 +4,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -28,27 +28,28 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun DayCard(
-    avgTemp : String,
-    maxTemp : String,
-    minTemp : String,
-    dayOfTheWeek : String,
-    dayAndMonth : String,
-    precipitation : String,
-    rainIcon : Painter,
-    weatherIcon : Painter
+    avgTemp: String,
+    maxTemp: String,
+    minTemp: String,
+    dayOfTheWeek: String,
+    dayAndMonth: String,
+    precipitation: String,
+    rainIcon: Painter,
+    weatherIcon: Painter
 ) {
     val fontColor = Color.Black
     Card(
         colors = CardDefaults.cardColors(containerColor = Color.White.copy(alpha = 0.6f)),
         modifier = Modifier
             .fillMaxWidth()
+            .height(50.dp)
     ) {
         Row(
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxSize()
                 .clip(shape = RoundedCornerShape(10.dp))
         ) {
-            Spacer(modifier = Modifier.width(2.dp))
+            Spacer(modifier = Modifier.width(10.dp))
             Text( //Average Temperature
                 text = avgTemp,
                 modifier = Modifier
@@ -81,20 +82,20 @@ fun DayCard(
                 color = fontColor,
                 fontSize = 13.sp
             )
-            Spacer(modifier = Modifier.width(150.dp))
+            Spacer(modifier = Modifier.width(120.dp))
 
             Column(modifier = Modifier.align(Alignment.CenterVertically)) {
                 Text(
                     text = dayOfTheWeek
                     /*   text = "${DateFormat.DAY_OF_WEEK_FIELD}"*/,
                     color = fontColor,
-                    fontSize = 10.sp
+                    fontSize = 14.sp
                 )
                 Text(
                     text = dayAndMonth
                     /* text = "${DateFormat.DATE_FIELD}"*/,
                     color = fontColor,
-                    fontSize = 10.sp
+                    fontSize = 12.sp
                 )
             }
         }
@@ -104,101 +105,88 @@ fun DayCard(
 @Preview
 @Composable
 fun WeekView() {
-    LazyColumn(
+    Column(
         modifier = Modifier
             .padding(8.dp)
             .wrapContentSize(Alignment.BottomCenter)
     ) {
-        item{ // You can change the number of cards as needed
-            DayCard(
-                avgTemp = "20°",
-                maxTemp = "22°",
-                minTemp = "16°",
-                dayOfTheWeek = "Monday",
-                dayAndMonth = "23/10",
-                precipitation = "30%",
-                rainIcon = painterResource(id = R.drawable.umbrella),
-                weatherIcon = painterResource(id = R.drawable.clouds)
-            )
-            Spacer(modifier = Modifier.height(8.dp)) // Add spacing between cards
-        }
-        item{ // You can change the number of cards as needed
-            DayCard(
-                avgTemp = "20°",
-                maxTemp = "22°",
-                minTemp = "16°",
-                dayOfTheWeek = "Tuesday",
-                dayAndMonth = "24/10",
-                precipitation = "30%",
-                rainIcon = painterResource(id = R.drawable.umbrella),
-                weatherIcon = painterResource(id = R.drawable.clouds)
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-        }
-        item{ // You can change the number of cards as needed
-            DayCard(
-                avgTemp = "20°",
-                maxTemp = "22°",
-                minTemp = "16°",
-                dayOfTheWeek = "Wednesday",
-                dayAndMonth = "25/10",
-                precipitation = "30%",
-                rainIcon = painterResource(id = R.drawable.umbrella),
-                weatherIcon = painterResource(id = R.drawable.clouds)
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-        }
-        item{ // You can change the number of cards as needed
-            DayCard(
-                avgTemp = "20°",
-                maxTemp = "22°",
-                minTemp = "16°",
-                dayOfTheWeek = "Thursday",
-                dayAndMonth = "26/10",
-                precipitation = "30%",
-                rainIcon = painterResource(id = R.drawable.umbrella),
-                weatherIcon = painterResource(id = R.drawable.clouds)
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-        }
-        item{ // You can change the number of cards as needed
-            DayCard(
-                avgTemp = "20°",
-                maxTemp = "22°",
-                minTemp = "16°",
-                dayOfTheWeek = "Friday",
-                dayAndMonth = "27/10",
-                precipitation = "30%",
-                rainIcon = painterResource(id = R.drawable.umbrella),
-                weatherIcon = painterResource(id = R.drawable.clouds)
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-        }
-        item{ // You can change the number of cards as needed
-            DayCard(
-                avgTemp = "20°",
-                maxTemp = "22°",
-                minTemp = "16°",
-                dayOfTheWeek = "Saturday",
-                dayAndMonth = "28/10",
-                precipitation = "30%",
-                rainIcon = painterResource(id = R.drawable.umbrella),
-                weatherIcon = painterResource(id = R.drawable.clouds)
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-        }
-        item{ // You can change the number of cards as needed
-            DayCard(
-                avgTemp = "20°",
-                maxTemp = "22°",
-                minTemp = "16°",
-                dayOfTheWeek = "Sunday",
-                dayAndMonth = "29/10",
-                precipitation = "30%",
-                rainIcon = painterResource(id = R.drawable.umbrella),
-                weatherIcon = painterResource(id = R.drawable.clouds)
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-        }
+        DayCard(
+            avgTemp = "20°",
+            maxTemp = "22°",
+            minTemp = "16°",
+            dayOfTheWeek = "Monday",
+            dayAndMonth = "23/10",
+            precipitation = "30%",
+            rainIcon = painterResource(id = R.drawable.umbrella),
+            weatherIcon = painterResource(id = R.drawable.clouds)
+        )
+        Spacer(modifier = Modifier.height(8.dp)) // Add spacing between cards
+        DayCard(
+            avgTemp = "20°",
+            maxTemp = "22°",
+            minTemp = "16°",
+            dayOfTheWeek = "Tuesday",
+            dayAndMonth = "24/10",
+            precipitation = "30%",
+            rainIcon = painterResource(id = R.drawable.umbrella),
+            weatherIcon = painterResource(id = R.drawable.clouds)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        DayCard(
+            avgTemp = "20°",
+            maxTemp = "22°",
+            minTemp = "16°",
+            dayOfTheWeek = "Wednesday",
+            dayAndMonth = "25/10",
+            precipitation = "30%",
+            rainIcon = painterResource(id = R.drawable.umbrella),
+            weatherIcon = painterResource(id = R.drawable.clouds)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        DayCard(
+            avgTemp = "20°",
+            maxTemp = "22°",
+            minTemp = "16°",
+            dayOfTheWeek = "Thursday",
+            dayAndMonth = "26/10",
+            precipitation = "30%",
+            rainIcon = painterResource(id = R.drawable.umbrella),
+            weatherIcon = painterResource(id = R.drawable.clouds)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        DayCard(
+            avgTemp = "20°",
+            maxTemp = "22°",
+            minTemp = "16°",
+            dayOfTheWeek = "Friday",
+            dayAndMonth = "27/10",
+            precipitation = "30%",
+            rainIcon = painterResource(id = R.drawable.umbrella),
+            weatherIcon = painterResource(id = R.drawable.clouds)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        DayCard(
+            avgTemp = "20°",
+            maxTemp = "22°",
+            minTemp = "16°",
+            dayOfTheWeek = "Saturday",
+            dayAndMonth = "28/10",
+            precipitation = "30%",
+            rainIcon = painterResource(id = R.drawable.umbrella),
+            weatherIcon = painterResource(id = R.drawable.clouds)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        DayCard(
+            avgTemp = "20°",
+            maxTemp = "22°",
+            minTemp = "16°",
+            dayOfTheWeek = "Sunday",
+            dayAndMonth = "29/10",
+            precipitation = "30%",
+            rainIcon = painterResource(id = R.drawable.umbrella),
+            weatherIcon = painterResource(id = R.drawable.clouds)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
     }
 }
--- app/src/main/java/com/example/vejrapp/appscreens/DayPage.kt
+++ app/src/main/java/com/example/vejrapp/appscreens/DayPage.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
@@ -54,4 +56,35 @@ fun DayPage(
         WeekView()
     }
 
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Preview
+fun DayPreview(){
+    Column {
+        DummySearchBar()
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            item {
+                TopWeather()
+            }
+            item {
+                CautionBox()
+            }
+            item {
+                LazyRowWithCards()
+            }
+            item {
+                DetailsBox()
+            }
+            item {
+                Spacer(modifier = Modifier.height(6.dp))
+                WeekView()
+            }
+        }
+
+
+    }
 }
