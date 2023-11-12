829739d46df325cff8b08d08df81eddbc118c446
S203932 <s203932@student.dtu.dk>
2023-11-12 18:16:39 +0100

    updated DayViewModel

--- app/src/main/java/com/example/vejrapp/presentation/day/DayViewModel.kt
+++ app/src/main/java/com/example/vejrapp/presentation/day/DayViewModel.kt
@@ -22,6 +22,43 @@ class DayViewModel @Inject constructor(
     private val _complete = MutableStateFlow(LocationforecastImplementation.defaultComplete)
     val complete = _complete.asStateFlow()
 
+
+    // Top half of dayPage information
+    private val currentWeather = complete.value.properties.timeseries.get(0)
+    val currentTemperature = currentWeather.data.instant?.details?.airTemperature
+    val currentCondition = currentWeather.data.nextOneHours?.summary?.symbolCode
+    val realFeel = currentWeather.data.instant?.details?.dewPointTemperature
+    val currentMinTemperature = currentWeather.data.nextOneHours?.details?.airTemperatureMin
+    val currentMaxTemperature = currentWeather.data.nextOneHours?.details?.airTemperatureMax
+    val currentPercentageRain =
+        currentWeather.data.nextOneHours?.details?.probabilityOfPrecipitation
+    val currentWindSpeed = currentWeather.data.instant?.details?.windSpeed
+
+
+    // No weather caution information in API data
+    // To make weather caution one must analyze the data oneself and issue warnings accordingly
+
+
+    // Hourly Data
+    val hourlyTemperature = MutableList<Float?>(24) { index ->
+        complete.value.properties.timeseries.get(index).data.instant?.details?.airTemperature
+    }
+    val hourlyCondition = MutableList<String>(24) { index ->
+        complete.value.properties.timeseries.get(index).data.nextOneHours?.summary?.symbolCode.toString()
+    }
+
+    val hourlyPercentageRain = MutableList<Float?>(24) { index ->
+        complete.value.properties.timeseries.get(index).data.nextOneHours?.details?.probabilityOfPrecipitation
+    }
+
+
+    // The middle of DayPage
+    // There is no visibility in API Data, can only say how much fog
+    val humidity = currentWeather.data.instant?.details?.relativeHumidity
+    val uVIndex = currentWeather.data.instant?.details?.ultravioletIndexClearSky
+    val pressure = currentWeather.data.instant?.details?.airPressureAtSeaLevel
+
+
     private val _city = MutableStateFlow(locations.defaultCity)
     val city = _city.asStateFlow()
 
