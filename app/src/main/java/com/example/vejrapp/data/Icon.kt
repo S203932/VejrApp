ba8051c226a45cb610fc8b8a6c5cbae5ffb58c94
Davidgtrivera <s215461@dtu.dk>
2023-11-24 00:10:19 +0100

    Update Icon.kt

--- app/src/main/java/com/example/vejrapp/data/Icon.kt
+++ app/src/main/java/com/example/vejrapp/data/Icon.kt
@@ -61,28 +61,21 @@ private fun isRowTransparent(bitmap: Bitmap, row: Int): Boolean {
 }
 
 fun getBitmapFromImage(context: Context, drawable: Int): Bitmap {
-
-    // on below line we are getting drawable
     val db = ContextCompat.getDrawable(context, drawable)
 
-    // in below line we are creating our bitmap and initializing it.
+    //create bitmap
     val bit = Bitmap.createBitmap(
         db!!.intrinsicWidth, db.intrinsicHeight, Bitmap.Config.ARGB_8888
     )
 
-    // on below line we are
-    // creating a variable for canvas.
+    // create a variable for canvas.
     val canvas = Canvas(bit)
 
-    // on below line we are setting bounds for our bitmap.
+    //set bounds for bitmap.
     db.setBounds(0, 0, canvas.width, canvas.height)
-
-    // on below line we are simply
-    // calling draw to draw our canvas.
     db.draw(canvas)
 
-    // on below line we are
-    // returning our bitmap.
+    // return bitmap.
     return bit
 }
 
