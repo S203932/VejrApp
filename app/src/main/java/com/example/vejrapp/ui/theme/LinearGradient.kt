9dc2c7d491494a8c1b0267951ff5d9b6cf40d6e5
KrisUrb <s163301@student.dtu.dk>
2024-01-17 12:41:17 +0100

    Update for active displaying background based on weathericons

--- app/src/main/java/com/example/vejrapp/ui/theme/LinearGradient.kt
+++ app/src/main/java/com/example/vejrapp/ui/theme/LinearGradient.kt
@@ -38,23 +38,67 @@ fun LinearGradient(weatherScreenViewModel: WeatherScreenViewModel) {
 
     // RGB values for #525252 and #333333
     // Nighttime theme:
-    val NighttimestartColor = Color(0xFF525252)
-    val NighttimeendColor = Color(0xFF333333)
+    val NighttimestartColor = Color(0xFFB4B6BE)
+    val NighttimeendColor = Color(0xFF91939A)
 
     //Evening theme:
-    val EveningstartColor = Color(0xFF40364E)
-    val EveningendColor = Color(0xFF282130)
+    val EveningstartColor = Color(0xFFFCCA5C)
+    val EveningendColor = Color(0xFF40364E)
 
     //Daytime theme:
     val DaystartColor = Color(0xFF43A6DB)
     val DayendColor = Color(0xFF2487BC)
 
     //Cloudy theme:
-    val CloudystartColor = Color(0xFF91939A)
-    val CloudyendColor = Color(0xFF585A5F)
+    val CloudystartColor = Color(0xFF43A6DB)
+    val CloudyendColor = Color(0xFF91939A)
 
+    /*
+    var gradient =
+        // Default to nighttime theme if the weather condition is not matched
+        Brush.linearGradient(
+            00.0f to Color.Magenta,
+            50.0f to Color.Cyan,
+            start = Offset(0f, 0f),
+            end = Offset(0f, Float.POSITIVE_INFINITY)
+        )
+
+    if (weatherImage.contains("day")) {
+        if (weatherImage.contains("clear")) {
+            gradient = Brush.linearGradient(
+                0.0f to DaystartColor,
+                1.0f to DayendColor,
+                start = Offset(0f, 0f),
+                end = Offset(0f, Float.POSITIVE_INFINITY)
+            )
+        }
+    } else if (weatherImage.contains("rain")) {
+        if (weatherImage.contains("snow")) {
+            gradient = Brush.linearGradient(
+                0.0f to CloudystartColor,
+                1.0f to CloudyendColor,
+                start = Offset(0f, 0f),
+                end = Offset(0f, Float.POSITIVE_INFINITY)
+            )
+        }
+    } else {
+        // Default to nighttime theme if the weather condition is not matched
+        gradient = Brush.linearGradient(
+            0.0f to DaystartColor,
+            1.0f to DayendColor,
+            start = Offset(0f, 0f),
+            end = Offset(0f, Float.POSITIVE_INFINITY)
+        )
+    }
+
+    Box(
+        modifier = Modifier
+            .background(gradient)
+            .fillMaxSize()
+    )
+}*/
     val gradient = when (imageRes) {
-        R.drawable.clearsky_day /*"clearsky_night", "clearsky_polartwilight", "fair_day", "fair_night", "fair_polartwilight"*/
+        R.drawable.clearsky_day, R.drawable.fair_day /*"clearsky_night", "clearsky_polartwilight", "fair_day", "fair_night", "fair_polartwilight"*/
         -> {
             Brush.linearGradient(
                 0.0f to DaystartColor,
@@ -64,19 +108,17 @@ fun LinearGradient(weatherScreenViewModel: WeatherScreenViewModel) {
             )
         }
 
-        /*"lightsnowshowers_day", "lightsnowshowers_night", "lightsnowshowers_polartwilight",
-        "heavysnowandthunder", "snow", "snowshowers_day", "snowshowers_night", "snowshowers_polartwilight",
-        "heavysnowshowersandthunder_day", "heavysnowshowersandthunder_night", "heavysnowshowersandthunder_polartwilight",
-        "heavysnowshowers_day", "heavysnowshowers_night", "heavysnowshowers_polartwilight",
-        "snowshowersandthunder_day", "snowshowersandthunder_night", "snowshowersandthunder_polartwilight",
-        "heavysnow", "sleet", "sleetshowers_day", "sleetshowers_night", "sleetshowers_polartwilight",
-        "heavysleetshowersandthunder_day", "heavysleetshowersandthunder_night", "heavysleetshowersandthunder_polartwilight",
-        "heavysleetshowers_day", "heavysleetshowers_night", "heavysleetshowers_polartwilight",
-        "sleetshowersandthunder_day", "sleetshowersandthunder_night", "sleetshowersandthunder_polartwilight",
-        "heavysleetandthunder", "lightsleet", "lightsleetshowersandthunder_day", "lightsleetshowersandthunder_night",
-        "lightsleetshowersandthunder_polartwilight", "lightsleetshowers_day", "lightsleetshowers_night", "lightsleetshowers_polartwilight",
-        */
-        R.drawable.partlycloudy_day -> {
+        R.drawable.partlycloudy_day, R.drawable.lightsnowshowers_day, R.drawable.cloudy, R.drawable.lightrainshowersandthunder_day,
+        R.drawable.lightrainshowersandthunder_polartwilight, R.drawable.lightrainshowers_day, R.drawable.lightsnowshowers_polartwilight,
+        R.drawable.heavyrainandthunder, R.drawable.heavysnowandthunder, R.drawable.rainandthunder, R.drawable.heavysleetshowers_day,
+        R.drawable.heavysnow, R.drawable.heavyrainshowers_day, R.drawable.lightsleet, R.drawable.heavyrain, R.drawable.lightrainshowers_day,
+        R.drawable.heavysleetshowers_day, R.drawable.lightsleetshowers_day, R.drawable.snow, R.drawable.heavyrainshowersandthunder_day,
+        R.drawable.snowshowers_day, R.drawable.snowshowersandthunder_day, R.drawable.fog, R.drawable.lightsnowandthunder, R.drawable.heavysleetandthunder,
+        R.drawable.lightrain, R.drawable.rainshowersandthunder_day, R.drawable.rain, R.drawable.lightsnow, R.drawable.lightsnow, R.drawable.heavysleet,
+        R.drawable.sleetandthunder, R.drawable.lightrainandthunder, R.drawable.sleet, R.drawable.lightssleetshowersandthunder_day, R.drawable.lightsleetandthunder,
+        R.drawable.partlycloudy_day, R.drawable.sleetshowersandthunder_day, R.drawable.rainshowers_day, R.drawable.snowandthunder, R.drawable.sleetshowers_day,
+        R.drawable.cloudy, R.drawable.heavysnowshowersandthunder_day, R.drawable.heavysnowshowers_day,
+        -> {
             Brush.linearGradient(
                 0.0f to CloudystartColor,
                 1.0f to CloudyendColor,
@@ -85,19 +127,31 @@ fun LinearGradient(weatherScreenViewModel: WeatherScreenViewModel) {
             )
         }
 
-        R.drawable.clearsky_day -> {
+        R.drawable.clearsky_polartwilight, R.drawable.fair_polartwilight, R.drawable.heavysleetshowersandthunder_polartwilight,
+        R.drawable.heavyrainshowers_polartwilight, R.drawable.lightrainshowers_polartwilight, R.drawable.heavysleetshowers_polartwilight,
+        R.drawable.lightsleetshowers_polartwilight, R.drawable.heavyrainshowersandthunder_polartwilight, R.drawable.snowshowers_polartwilight,
+        R.drawable.snowshowersandthunder_polartwilight, R.drawable.rainshowersandthunder_polartwilight, R.drawable.lightrainshowersandthunder_polartwilight,
+        R.drawable.lightssleetshowersandthunder_polartwilight, R.drawable.partlycloudy_polartwilight, R.drawable.sleetshowersandthunder_polartwilight,
+        R.drawable.rainshowers_polartwilight, R.drawable.sleetshowers_polartwilight, R.drawable.heavysnowshowersandthunder_polartwilight,
+        R.drawable.heavysnowshowers_polartwilight
+        -> {
             Brush.linearGradient(
-                0.0f to NighttimestartColor,
-                1.0f to NighttimeendColor,
+                0.0f to EveningstartColor,
+                1.0f to EveningendColor,
                 start = Offset(0f, 0f),
                 end = Offset(0f, Float.POSITIVE_INFINITY)
             )
         }
 
-        R.drawable.clearsky_day/*, R.drawable.lightssnowshowersandthunder_polartwilight*/ -> {
+        R.drawable.clearsky_night, R.drawable.lightssnowshowersandthunder_night, R.drawable.lightrainshowers_night, R.drawable.heavysleetshowersandthunder_night,
+        R.drawable.heavyrainshowers_night, R.drawable.lightrainshowers_night, R.drawable.heavysleetshowers_night, R.drawable.lightsleetshowers_night,
+        R.drawable.heavyrainshowersandthunder_night, R.drawable.snowshowers_night, R.drawable.snowshowersandthunder_night, R.drawable.rainshowersandthunder_night,
+        R.drawable.lightrainshowersandthunder_night, R.drawable.lightssnowshowersandthunder_night, R.drawable.partlycloudy_night, R.drawable.sleetshowersandthunder_night,
+        R.drawable.rainshowers_night, R.drawable.sleetshowers_night, R.drawable.heavysnowshowersandthunder_night, R.drawable.heavysnowshowers_night
+        -> {
             Brush.linearGradient(
-                0.0f to EveningstartColor,
-                1.0f to EveningendColor,
+                0.0f to NighttimestartColor,
+                1.0f to NighttimeendColor,
                 start = Offset(0f, 0f),
                 end = Offset(0f, Float.POSITIVE_INFINITY)
             )
